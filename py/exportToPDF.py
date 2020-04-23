101f61ed8f7fa1a835a0fbad0e13d62614fd2bb4
Dimitar Grudev <dimitar.grudev@hedgehoglab.com>
2020-04-23 13:11:50 +0300

    update April 2020

--- py/exportToPDF.py
+++ py/exportToPDF.py
@@ -1,5 +1,11 @@
 import pdfkit
 
+# 1. Run in terminal: sudo easy_install pip
+# 2. Then do: brew install Caskroom/cask/wkhtmltopdf
+# 3. Adjust file paths from the current file below
+# 4. Go to the same directory where this python script exists on your mac
+# 5. Run: python exportToPDF.py
+
 # using this script https://github.com/JazzCore/python-pdfkit
 
 options = {
@@ -7,4 +13,4 @@ options = {
     'no-outline': None
 }
 
-pdfkit.from_file('/Volumes/data/PROJECTS/dimitargrudev.github.io/index.html', '/Volumes/data/PROJECTS/dimitargrudev.github.io/pdf/dimitarGrudevCV.pdf', options=options)
+pdfkit.from_file('/Users/grudev/data/PROJECTS/grudev.github.io/index-pdf.html', '/Users/grudev/data/PROJECTS/grudev.github.io/pdf/dimitarGrudevCV.pdf', options=options)
